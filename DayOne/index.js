c8bb4ad94e4e4257e679365bf1122096afd64f56
royanxdev <nurmuhammadroyanbirallah@gmail.com>
2026-01-06 22:25:49 +0700

    First Commit

---
+++ DayOne/index.js
@@ -0,0 +1,17 @@
+// Menampilkan Log ke konsol
+console.log("Hello World");
+
+// Variable dan Konstanta
+// let (dapat diubah)
+// const (tidak dapat diubah)
+
+// Tipe Data String
+let nama = "Royan";
+console.log("Hello nama saya " + nama);
+
+// Tipe Data Integer
+let angka = 10;
+console.log("Ini angka " + angka);
+
+// Begitulah tipe data di js masih banyak lagi pokoknya cuman salah satu implementasinya seperti itu.
+
