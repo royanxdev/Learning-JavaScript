32f5286c5a851569ad6bb48c64d31bbf9c4523e0
royanxdev <nurmuhammadroyanbirallah@gmail.com>
2026-01-17 17:13:24 +0700

    DayTwo

---
+++ DayTwo/index.js
@@ -0,0 +1,12 @@
+// Memastikan tipe data ini angka atau yang lainya menggunakan typeoff
+    /*let Angka = 123;
+
+    if (typeof Angka === "number")
+    {
+        console.log("Ini Angka");
+    }else
+    {
+        console.log("Ini bukan Angka");
+    }*/
+
+        
