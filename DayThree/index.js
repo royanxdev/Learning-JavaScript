365311bc1829c083a67ddcc883179ad4c44a813f
royanxdev <nurmuhammadroyanbirallah@gmail.com>
2026-01-18 13:07:39 +0700

    Day Three

--- DayThree/index.js
+++ DayThree/index.js
@@ -30,3 +30,28 @@
     console.log(`${perempuan.name}`);
     perempuan.ganteng();
     */
+
+    /*
+    const Submit = document.getElementById("Submit").addEventListener("click", function(){
+        const FirstName = document.getElementById("FirstName").value;
+        const LastName = document.getElementById("LastName").value;
+        const Years = document.getElementById("Umur").value;
+        const Prodi = document.getElementById("ProgramStudi").value;
+
+        const Identitas ={
+            FirstName: FirstName,
+            LastName: LastName,
+            FullName: function(FirstName, LastName){
+                return this.FirstName + " " + this.LastName;
+            },
+            Years: Years,
+            ProgramStudi: Prodi, 
+        }
+        
+        console.log(`Nama Saya: ${Identitas.FullName()}`);
+
+        Identitas.Age = 10;
+
+        console.log(`Umur Saya ${Identitas.Age}`);
+    })
+    */
