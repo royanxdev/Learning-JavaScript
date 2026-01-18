db6e8de360d786d9b572c296db2b839d72579769
royanxdev <nurmuhammadroyanbirallah@gmail.com>
2026-01-18 18:43:12 +0700

    stack

---
+++ DayThree/stack.js
@@ -0,0 +1,13 @@
+// Belajar stack 
+const MyStack = [];
+
+MyStack.push(`a`); /*push digunakan untuk menambahkan element array baru*/
+MyStack.push(`b`);
+
+console.log(MyStack);
+
+console.log(MyStack.pop()); /* pop digunakan untuk melihat array yang paling terbaru di tambahkan*/
+
+const bom = window.innerHeight;
+
+console.log(bom);
